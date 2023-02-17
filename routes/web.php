a1a8a069d5e1b9d40738957009c840d20eba12c2
Askar Rymbayev <askar.rymbayev@gmail.com>
2023-02-17 21:42:54 +0600

    week 15, lesson 3

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Models\Post;
 use \App\Http\Controllers\PostController;
 
 use \Illuminate\Support\Facades\File;
+use \Illuminate\Http\Request;
+use \Illuminate\Http\Response;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,6 +17,90 @@ use \Illuminate\Support\Facades\File;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function (Request $request) {
+//    $cart = session('cart');
+//    if (!is_array($cart)) {
+//        session()->put('cart', []);
+//    }
+//    session()->put('test1', 1);
+//    session(['two'=>2]);
+//    session()->put('null_value', true);
+    echo '<pre>';
+
+//    $productId = 3;
+//    $command = 'increment';
+//    $cartProductKey = 'cart.' . $productId . '.0.count';
+//
+//    var_dump($cartProductKey);
+//
+//    if (session()->has($cartProductKey)) {
+//        if ($command == 'increment') {
+//            session()->increment($cartProductKey);
+//        } elseif ($command == 'decrement') {
+//            session()->decrement($cartProductKey);
+//        }
+//    }
+
+//    $data = [
+//        'product_id' => 1,
+//        'count' => 2,
+//    ];
+//    $cartKey = 'cart.' . $data['product_id'];
+//
+//    if (session()->has('cart.' . $data['product_id'] . '.0')) {
+//        return 'ok';
+//    } else {
+//        session()->push($cartKey, $data);
+//    }
+
+
+//    session()->flash('flash_data_1', 100);
+//    session()->flash('flash_data_2', 200);
+//    session()->flash('flash_data_3', 300);
+//    session()->keep('flash_data_2');
+//    session()->reflash();
+
+//    session()->forget(['null_value', 'test1', 'two']);
+    session()->flush();
+
+
+
+//    dd($request->session()->all());
+
+//    var_dump(session()->get('cart'));
+
+//    var_dump(session()->all());
+//    $test1 = session()->pull($cartKey);
+//    var_dump($test1);
+    var_dump(session()->all());
+
+//    var_dump(session()->has('null_value'));
+//    var_dump(session()->missing('null_value'));
+//    var_dump(session()->exists('null_value'));
+    echo '</pre>';
+//    session(['cart'=>[]]);
+//    dd($session);
+    return 'done';
+});
+
+Route::get('/set_cookie', function (Request $request) {
+    $minutes = 3;
+    $response = new Response('Set Cookie');
+    $response->withCookie(cookie()->forever('userToken', md5('sdfs')));
+
+    return $response;
+});
+
+Route::get('/get_cookie', function (Request $request) {
+    $value = $request->cookie('userToken');
+
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+
+    return 'done';
+});
 
 Route::resource('posts', PostController::class)->withTrashed();
 Route::get('/posts/{post}/restore', [PostController::class, 'restore'])->withTrashed()->name('posts.restore');
